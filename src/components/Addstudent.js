65bee2041ac24d426bb24e186655b4a1d480c17b
Mahima1111 <mahimavijayan123@gmail.com>
2022-05-26 11:23:23 +0530

    addstudent completed

--- src/components/Addstudent.js
+++ src/components/Addstudent.js
@@ -1,3 +1,10 @@
+import React from 'react'
+
+const Addstudent = () => {
+  return (
+    <div>
+
+
 <div className="container">
     <div className="row">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
@@ -24,3 +31,8 @@
         </div>
     </div>
 </div>
+    </div>
+  )
+}
+
+export default Addstudent
